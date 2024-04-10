4379a665b31e64e58d13fea1b101418d97b691f9
Chenping <chenpingguo3@gmail.com>
2024-04-10 19:12:35 +0200

    commit

--- campeones.js
+++ campeones.js
@@ -1,4 +1,5 @@
 var botones;
+var contenedorDeBotones;
 
 function char_inf() {
     window.location.href = 'char_inf.html';
@@ -18,11 +19,12 @@ function splashArts() {
 
 document.addEventListener("DOMContentLoaded", function() {
     // Obtener todos los botones
+    //contenedorDeBotones = document.getElementById("box_campeones");
     botones = document.querySelectorAll("button");
-
+    
     // Agregar evento de clic al botón de filtro
     document.getElementById("filtro1").addEventListener("click", function() {
-        filtrarBotonesPorID("Fighter");
+        filtrarBotonesPorID("Fighter") ;
     });
 
     document.getElementById("filtro2").addEventListener("click", function() {
@@ -47,12 +49,13 @@ document.addEventListener("DOMContentLoaded", function() {
         // Ocultar todos los botones
         botones.forEach(function(boton) {
             var clasesBoton = boton.className.split(" ");
-            if (clasesBoton.includes(id)) {
+            if (clasesBoton.includes("header_letter") || clasesBoton.includes("box") || clasesBoton.includes(id)) {
                 boton.style.display = "block";
             } else {
                 boton.style.display = "none";
             }
-        });
+        })
+        ;
         
         /*botones.forEach(function(boton) {
             if (boton.getAttribute("id") === id){
